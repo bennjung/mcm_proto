9a3fdbf6614d1e8e62ae2bdb0d68d89eb3cf3acb
sapgun <jaeuk0701@gmail.com>
2025-04-06 10:24:49 +0900

    feat: Next.js 앱 디렉토리 구조 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { GitHubAnalyzer } from '@/components/GitHubAnalyzer'
+'use client';
+import React from 'react';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <GitHubAnalyzer />
+      <div>GitHub Analyzer</div>
     </main>
   )
-} 
+}
